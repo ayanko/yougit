d6e26ddc34a5fdfad577f0f3f10636b6c2ab2e3b
Andriy Yanko <andriy.yanko@gmail.com>
2009-06-21 18:30:59 +0300

    Terminate application if no config file

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -3,10 +3,14 @@ require 'ostruct'
 require 'yaml'
 
 # Load application configuration
+config_file = File.join(RAILS_ROOT, "config", "application.yml")
+
+abort("Please create #{config_file}") unless File.exists?(config_file)
+
 AppConfig = OpenStruct.new(
   YAML.load(
     ERB.new(
-      File.read(File.join(RAILS_ROOT, "config", "application.yml"))
+      File.read(config_file)
     ).result
   )[ENV['RAILS_ENV']]
 )
